07105d6eb749ab070f68fcb13d76abe4376537ea
Thembek <andile.goba@younglings.africa>
2024-10-03 08:51:14 +0200

    using Dimensions api for images on phone screen

--- app/GameOverScreen.js
+++ app/GameOverScreen.js
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import Title from '../components/ui/Title';
 import Colors from '../constants/colors';
@@ -19,6 +19,8 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}){
 
 export default GameOverScreen;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
     rootContainer: {
         flex: 1,
@@ -27,8 +29,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     imageContainer: {
-        width: 300,
-        height: 300,
+        width: deviceWidth < 380 ? 150 : 300,
+        height: deviceWidth < 380 ? 150 : 300,
         borderRadius: 150,
         borderWidth: 3,
         borderColor: Colors.primary800,
